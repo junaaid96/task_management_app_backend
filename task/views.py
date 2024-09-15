f76e01abb3819e6307224b7079f8c56502602eca
junaaid96 <info.junaidulislam@gmail.com>
2024-09-15 16:55:06 +0600

    v0.3.1

--- task/views.py
+++ task/views.py
@@ -1,16 +1,28 @@
-from rest_framework import viewsets
+from rest_framework import viewsets, permissions
 from .models import Task, Subtask
 from .serializers import TaskSerializer, SubtaskSerializer
-from task_management_app_backend.permissions import IsAdmin
+from task_management_app_backend.permissions import IsAdmin, IsUser
 
 
 class TaskViewSet(viewsets.ModelViewSet):
     queryset = Task.objects.all()
     serializer_class = TaskSerializer
-    permission_classes = [IsAdmin]
+
+    def get_permissions(self):
+        if self.action in ['create', 'update', 'partial_update', 'destroy']:
+            permission_classes = [IsAdmin]
+        else:
+            permission_classes = [permissions.AllowAny]
+        return [permission() for permission in permission_classes]
 
 
 class SubtaskViewSet(viewsets.ModelViewSet):
     queryset = Subtask.objects.all()
     serializer_class = SubtaskSerializer
-    permission_classes = [IsAdmin]
+
+    def get_permissions(self):
+        if self.action in ['create', 'update', 'partial_update', 'destroy']:
+            permission_classes = [IsAdmin]
+        else:
+            permission_classes = [permissions.AllowAny]
+        return [permission() for permission in permission_classes]
--- task_management_app_backend/urls.py
+++ task_management_app_backend/urls.py
@@ -19,6 +19,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', include('task.urls')),
-    path('api/', include('users.urls')),
+    path('tasks/', include('task.urls')),
+    path('users/', include('users.urls')),
 ]
